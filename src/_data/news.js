40c92d010b7d1f431cc946d1c3fed4d924e82f3e
Kurt Hansen <kurt.hansen@cuanswers.com>
2020-11-05 14:41:07 -0500

    redirects

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,15 +1,30 @@
 const axios = require('axios');
+const countries = require("./countries.json");
 
-console.log(require('dotenv').config()); // log in bash output
+// console.log(require('dotenv').config()); // log in bash output
+require('dotenv').config();
 
-module.exports = async function() {
+async function getNews(country) {
   try {
 
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`); // note the backticks!
-    return response.data;
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`); // note the backticks!
+    return  {
+    	"country": country,
+    	"articles": response.data.articles
+    }
   
   } catch (error) {
 
     console.error(error);
   }
+};
+
+module.exports = async function() {
+
+	var newsPromises = countries.map(getNews);
+
+	return Promise.all(newsPromises).then(newsObjects => {
+		//console.log('newsObjects:', newsObjects); // log in bash output
+		return [].concat.apply([],newsObjects);
+	});
 };
